3f8e7ed63a6ef5db0f66090a91fe42eefa4cbaa2
flyingdelta <flyingdelta@outlook.com>
2022-06-28 19:28:53 +0530

    created stack in Java

---
+++ inJava/stack/stack.java
@@ -0,0 +1,23 @@
+package inJava.stack;
+
+class stackA {
+    int maxLength = 100;
+    private int[] data = new int[maxLength];
+    private int k = 0;
+    int peek(){
+        if (this.k == 0) throw new ArrayIndexOutOfBoundsException();
+        return data[k - 1];
+    }
+    int push(int val){
+        if (this.k == maxLength) throw new ArrayIndexOutOfBoundsException();
+        this.data[this.k] = val;
+        this.k += 1; 
+        return this.k + 1;
+    }
+    int pop(){
+        if (this.k == 0) throw new ArrayIndexOutOfBoundsException();
+        int val = this.data[this.k - 1];
+        this.k -= 1;
+        return val;
+    }
+}
