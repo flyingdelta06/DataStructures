808d8447563bb524d395a56ad5552fc6ed130aa1
flyingdelta <flyingdelta@outlook.com>
2022-06-30 19:13:51 +0530

    made methods public and formatted code

--- inJava/tree/binaryTree/binaryTree.java
+++ inJava/tree/binaryTree/binaryTree.java
@@ -2,53 +2,74 @@ package inJava.tree.binaryTree;
 
 import java.util.NoSuchElementException;
 
-class binaryTree {
+public class binaryTree {
     private Node root = null;
-    private void inorderT(Node a){
-        if (a.left != null)  inorderT(a.left);
-        System.out.printf("%d, ", a.key);
-        if (a.right != null) inorderT(a.right);
+
+    binaryTree() {
     }
-    private Node search(int key){
+
+    public binaryTree(int... elements) {
+        for (int i = 0; i < elements.length; i++) {
+            this.insert(i);
+        }
+    }
+
+    private void inorderT(Node a) {
+        if (a.left != null)
+            inorderT(a.left);
+        System.out.printf("%d, ", a.key);
+        if (a.right != null)
+            inorderT(a.right);
+    }
+
+    private Node search(int key) {
         Node currNode = this.root;
         while (key != currNode.key) {
-            if (key < currNode.key) currNode = currNode.left;
-            else if (key > currNode.key) currNode = currNode.right;
-            if (currNode == null){
+            if (key < currNode.key)
+                currNode = currNode.left;
+            else if (key > currNode.key)
+                currNode = currNode.right;
+            if (currNode == null) {
                 return null;
             }
         }
         return currNode;
     }
-    private Node getSuccessor(Node a){
+
+    private Node getSuccessor(Node a) {
         Node temp = a.right;
-        while (temp.left != null) temp = temp.left;
+        while (temp.left != null)
+            temp = temp.left;
         return temp;
     }
-    void insert(int key){
+
+    public void insert(int key) {
         Node newNode = new Node(key);
-        if (this.root == null){
+        if (this.root == null) {
             this.root = newNode;
             return;
         }
         Node currNode = this.root;
-        while (currNode != null){
+        while (currNode != null) {
             if (key <= currNode.key) {
-                if (currNode.left == null){
+                if (currNode.left == null) {
                     newNode.parent = currNode;
                     currNode.left = newNode;
                     break;
-                } else currNode = currNode.left;
+                } else
+                    currNode = currNode.left;
             } else {
-                if (currNode.right == null){
+                if (currNode.right == null) {
                     newNode.parent = currNode;
                     currNode.right = newNode;
                     break;
-                } else currNode = currNode.right;
+                } else
+                    currNode = currNode.right;
             }
         }
     }
-    void inorder(){
+
+    public void inorder() {
         if (this.root == null) {
             System.out.println("The tree is empty...");
             return;
@@ -57,18 +78,21 @@ class binaryTree {
         inorderT(this.root);
         System.out.printf("\b\b] \n");
     }
-    void delete(int key){
+
+    public void delete(int key) {
         Node toDelete = search(key);
-        if (toDelete == null) throw new NoSuchElementException("No such element found!");
+        if (toDelete == null)
+            throw new NoSuchElementException("No such element found!");
         if (toDelete.left != null && toDelete.right != null) {
             Node y = getSuccessor(toDelete);
-            if (y == toDelete.right){
+            if (y == toDelete.right) {
                 if (toDelete.parent != null) {
                     y.left = toDelete.left;
                     y.left.parent = y;
-                    if (toDelete.parent.right == toDelete){
+                    if (toDelete.parent.right == toDelete) {
                         toDelete.parent.right = y;
-                    } else toDelete.parent.left = y;
+                    } else
+                        toDelete.parent.left = y;
                     y.parent = toDelete.parent;
                 } else {
                     this.root = y;
@@ -80,24 +104,25 @@ class binaryTree {
                 Node r = toDelete.right;
                 Node x = y.right;
                 r.left = x;
-                if (x != null) x.parent = r;
+                if (x != null)
+                    x.parent = r;
                 y.right = r;
-                if (x != null) r.parent = y;
+                if (x != null)
+                    r.parent = y;
                 y.left = toDelete.left;
                 y.left.parent = y;
                 if (toDelete.parent != null) {
                     toDelete.parent.right = y;
                     y.parent = toDelete.parent;
-                }
-                else {
+                } else {
                     this.root = y;
                     y.left = toDelete.left;
                     y.left.parent = y;
                 }
             }
-        } else if (toDelete.left != null || toDelete.right != null){
-            Node subTree = (toDelete.left != null)? toDelete.left : toDelete.right;
-            if (toDelete.parent != null){
+        } else if (toDelete.left != null || toDelete.right != null) {
+            Node subTree = (toDelete.left != null) ? toDelete.left : toDelete.right;
+            if (toDelete.parent != null) {
                 if (toDelete.parent.right == toDelete) {
                     toDelete.parent.right = subTree;
                     subTree.parent = toDelete.parent;
@@ -113,23 +138,32 @@ class binaryTree {
             if (toDelete.parent != null) {
                 if (toDelete.parent.right == toDelete) {
                     toDelete.parent.right = null;
-                } else toDelete.parent.left = null;
-            } else this.root = null;
+                } else
+                    toDelete.parent.left = null;
+            } else
+                this.root = null;
         }
     }
+
+    public Node root() {
+        return this.root;
+    }
+
     // int[] toList();
 
 }
 
-final class Node{
+final class Node {
     Node parent = null;
     Node left = null;
     Node right = null;
     int key = 0;
-    Node(int val){
+
+    Node(int val) {
         this.key = val;
     }
-    Node(int val, Node parent, Node left, Node right){
+
+    Node(int val, Node parent, Node left, Node right) {
         this.key = val;
         this.left = left;
         this.right = right;
